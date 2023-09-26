4decdb3e24de94fab31fb689b2a2687e8a1c15d6
samirfdez <samirfdezc@gmail.com>
2023-09-26 17:41:11 -0400

    create my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import background from '../public/img/construction.jpg'
+
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
