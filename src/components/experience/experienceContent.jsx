02aaa630e87a1f21107a2182fe75a84830f208c8
samirfdez <samirfdezc@gmail.com>
2023-10-03 16:29:19 -0400

    ++

---
+++ src/components/experience/experienceContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Education } from './education'
+import { Experience } from './experience'
+
+export const ExperienceContent = () => {
+  return (
+    <>
+        <Education/>
+        <Experience/>
+    </>
+  )
+}
--- src/configs/experience.config.jsx
+++ src/configs/experience.config.jsx
@@ -1,11 +1,70 @@
 export const education = [
-    {
-
-    }
-]
+  {
+    institution: "Universidad APEC",
+    course: "Ingeniería en Software",
+    year: "2018-present",
+  },
+  {
+    institution: "Instituto Tecnológico de Las Américas",
+    course: "Tecnólogo en Desarrollo de Software",
+    year: "2016 - 2018",
+  },
+  {
+    institution: "Instituto Politecnico Pilar Constanzo",
+    course: "Technical Bachelor in Computing",
+    year: "2011-2015",
+  },
+];
 
 export const experience = [
-    {
-        
-    }
-]
+  {
+    position: "Junior Front-end Developer",
+    year: "2023-present",
+    empresa: "Signos Framework",
+    responsibilities: [
+      "Web systems design and coding",
+      "Performance improvement",
+      "Testing and debugging",
+      "Compatibility, update and maintenance",
+    ],
+  },
+  {
+    position: "Freelance Developer",
+    year: "2021-2022",
+    empresa: "",
+    responsibilities: [
+      "Creation of landing pages for businesses",
+      "Website remodeling",
+      "Program specific components or modules",
+    ],
+  },
+  {
+    position: "Developer",
+    year: "2020 - 2021",
+    empresa: "recursosdigitalesrde.org",
+    responsibilities: [
+      "Creation of digital resources",
+      "Schedule textbook activities to dynamic content",
+    ],
+  },
+  {
+    position: "Computer Technician",
+    year: "2018-2020",
+    empresa: "ISFODOSU",
+    responsibilities: [
+      "Assist department staff",
+      "Manage student database",
+      "Service to students",
+    ],
+  },
+  {
+    position: "Computer Technician",
+    year: "2015",
+    empresa: "Grupo Viamar",
+    responsibilities: [
+      "Fix and replace hardware to all kinds of computers",
+      "Provide functional and technical support",
+      "Follow up on Employee cases",
+    ],
+  },
+];
