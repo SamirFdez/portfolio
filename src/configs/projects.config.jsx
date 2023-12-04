1f5eb7882fbfe0cd3ca244dbccfdcb6e28c1835f
Samir Fernandez <samirfdezc@gmail.com>
2023-12-04 12:03:21 -0400

    Portfolio ready

--- src/configs/projects.config.jsx
+++ src/configs/projects.config.jsx
@@ -9,46 +9,46 @@ import todoList from "../assets/projects/todoList.jpg";
 import recursoDigitales from "../assets/projects/recursoDigitales.png"
 
 export const projectsArray = [
-  {
-    name: "Todo List",
-    image: todoList,
-    description:
-      "App that allows you to organize and manage your tasks efficiently. Record your pending tasks, mark completed tasks and maintain full control of your daily activities.",
-    stacks: [
-      {
-        name: "React",
-        icon: reactJs,
-      },
-      {
-        name: "JS",
-        icon: js,
-      },
-      {
-        name: "HTML",
-        icon: html,
-      },
-      {
-        name: "CSS",
-        icon: css,
-      },
-      {
-        name: "Bootstrap",
-        icon: bootstrap,
-      },
-    ],
-    share: [
-      {
-        platform: "web",
-        icon: iconWeb,
-        link: "https://todolist-samirfdez.vercel.app",
-      },
-      {
-        platform: "gitHub",
-        icon: iconGithub,
-        link: "https://github.com/SamirFdez/todoList",
-      },
-    ],
-  },
+  // {
+  //   name: "Todo List",
+  //   image: todoList,
+  //   description:
+  //     "App that allows you to organize and manage your tasks efficiently. Record your pending tasks, mark completed tasks and maintain full control of your daily activities.",
+  //   stacks: [
+  //     {
+  //       name: "React",
+  //       icon: reactJs,
+  //     },
+  //     {
+  //       name: "JS",
+  //       icon: js,
+  //     },
+  //     {
+  //       name: "HTML",
+  //       icon: html,
+  //     },
+  //     {
+  //       name: "CSS",
+  //       icon: css,
+  //     },
+  //     {
+  //       name: "Bootstrap",
+  //       icon: bootstrap,
+  //     },
+  //   ],
+  //   share: [
+  //     {
+  //       platform: "web",
+  //       icon: iconWeb,
+  //       link: "https://todolist-samirfdez.vercel.app",
+  //     },
+  //     {
+  //       platform: "gitHub",
+  //       icon: iconGithub,
+  //       link: "https://github.com/SamirFdez/todoList",
+  //     },
+  //   ],
+  // },
   {
     name: "Todo List",
     image: todoList,
