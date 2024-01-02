579a5a17f98f56591055331d6dda542c0a50ab1a
Samir Fernandez <samirfdezc@gmail.com>
2024-01-02 13:08:39 -0400

    ++

--- src/configs/projects.config.jsx
+++ src/configs/projects.config.jsx
@@ -2,7 +2,7 @@ import html from "../assets/icons/html-5.svg";
 import css from "../assets/icons/css-3.svg";
 import js from "../assets/icons/javascript.svg";
 import reactJs from "../assets/icons/react.svg";
-import tailwind from "../assets/icons/tailwindcss-icon.svg"
+import tailwind from "../assets/icons/tailwindcss-icon.svg";
 import bootstrap from "../assets/icons/bootstrap.svg";
 import iconGithub from "../assets/images/iconGithub.png";
 import iconWeb from "../assets/images/iconWeb.png";
@@ -11,46 +11,6 @@ import todoList from "../assets/projects/todoList.jpg";
 import recursoDigitales from "../assets/projects/recursoDigitales.png";
 
 export const projectsArray = [
-  // {
-  //   name: "Todo List",
-  //   image: todoList,
-  //   description:
-  //     "App that allows you to organize and manage your tasks efficiently. Record your pending tasks, mark completed tasks and maintain full control of your daily activities.",
-  //   stacks: [
-  //     {
-  //       name: "React",
-  //       icon: reactJs,
-  //     },
-  //     {
-  //       name: "JS",
-  //       icon: js,
-  //     },
-  //     {
-  //       name: "HTML",
-  //       icon: html,
-  //     },
-  //     {
-  //       name: "CSS",
-  //       icon: css,
-  //     },
-  //     {
-  //       name: "Bootstrap",
-  //       icon: bootstrap,
-  //     },
-  //   ],
-  //   share: [
-  //     {
-  //       platform: "web",
-  //       icon: iconWeb,
-  //       link: "https://todolist-samirfdez.vercel.app",
-  //     },
-  //     {
-  //       platform: "gitHub",
-  //       icon: iconGithub,
-  //       link: "https://github.com/SamirFdez/todoList",
-  //     },
-  //   ],
-  // },
   {
     name: "Movie World",
     image: movieWorld,
@@ -74,7 +34,7 @@ export const projectsArray = [
         icon: css,
       },
       {
-        name: "bootstrap",
+        name: "tailwind",
         icon: tailwind,
       },
     ],
